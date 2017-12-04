69d5fadd8e303f76f38ceafaad91350b957b6e5b
Monika Williams <monika.m.williams@gmail.com>
2017-12-04 08:54:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// Write your solution in this file!
-var customerName = "bob";
+ var customerName = 'bob';
 
-function upperCaseCustomerName () {
-  return customerName.toUpperCase();
-}
+ function upperCaseCustomerName() {
+   return customerName.toUppercase();
+ }
 
-function setBestCustomer() {
-  var bestCustomer = "not bob";
-  }
+ function setBestCustomer(){
+   var bestCustomer = 'not bob';
+ }
+ function overwriteBestCustomer(){
+   var bestCustomer = 'maybe bob';
+ }
